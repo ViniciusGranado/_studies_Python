d89b445abeb1be0795152c8bd21747e8368c9e6c
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-04-25 16:54:45 -0300

    Added solving of challenge 003

---
+++ _challenges/challenge003/README.md
@@ -0,0 +1,4 @@
+# Challenge003
+Criar um programa que pergunta dois números para o usuário e mostra a soma dos mesmos.
+
+Create a program that asks the user two numbers, and show their sum.
---
+++ _challenges/challenge003/challenge003.py
@@ -0,0 +1,6 @@
+print('======DESAFIO 03======')
+number1 = int(input('Digite um número: '))
+number2 = int(input('Digite outro número: '))
+sum_numbers = number1+number2
+print('A soma de',number1,'e',number2,'é',sum_numbers)
+              
