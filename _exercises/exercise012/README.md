db3d832256bc5a86eb14079d6849ac1d5bfd8d9a
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-04-30 17:20:45 -0300

    Added solving of challenge 012

---
+++ _exercises/exercise012/README.md
@@ -0,0 +1,8 @@
+# Exercício 012
+Criar um algorítmo que pergunta ao usuário o valor de um
+produto e um valor em porcentagem que será subtraído do
+produto. Mostra o valor final após o desconto.
+
+Create an algorithm that asks the user a product value and 
+a percentage that will be subtracted from the product. Show
+the final value after the discount.
---
+++ _exercises/exercise012/exercise012.py
@@ -0,0 +1,31 @@
+def validate_value(number_str):
+    number_is_valid = True
+    for i in number_str:
+        if not (i.isnumeric() or i=='.') or (number_str[-1]=='.'):
+            number_is_valid = False
+
+    return number_is_valid
+
+
+def get_values(i):
+    is_valid = False
+    message = ['Qual é o valor do produto? R$', 'Qual a porcentagem de desconto? %']
+    while not is_valid:
+        number_str = input(message[i]).replace(',', '.')
+        is_valid = validate_value(number_str)
+
+        if is_valid:
+            converted_number = float(number_str)
+            return converted_number
+        else:
+            print('Número inválido, tente novamente.')
+
+
+product_value = get_values(0)
+discount = get_values(1)
+final_value = (product_value) - (product_value*discount/100)
+
+formatted_product_value = str("%.2f" % product_value).replace('.', ',')
+formatted_final_value = str("%.2f" % final_value).replace('.', ',')
+
+print('\nO produto que custava R${}, na promoção com desconto de {}% irá custar RS{}'.format(formatted_product_value, discount, formatted_final_value))
