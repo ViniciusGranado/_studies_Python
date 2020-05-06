59014ad9cc315e3c690a7fff6edd64ae132b6cba
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-05-06 14:28:17 -0300

    Added solving of exercise 042

---
+++ _exercises/exercise042/README.md
@@ -0,0 +1,12 @@
+# Exercício 042
+
+Refaça o exercício 035, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+- EQUILÁTERO: todos os lados iguais.
+- ISÓSCELES: dois lados iguais, um diferente.
+- ESCALENO: todos os lados diferentes.
+
+Redo exercise 035, adding the feature to show what type of triangle will be formed:
+- EQUILATERAL: All sides are equal.
+- ISOSCELES: Two sides are equal.
+- SCALENE: All sides are different.
+
---
+++ _exercises/exercise042/exercise042.py
@@ -0,0 +1,53 @@
+def validate_float_number(number_str):
+    is_valid = True
+
+    if number_str == '' or number_str[-1] == '.':
+        is_valid = False
+    else:
+        for i in number_str:
+            if not (i.isnumeric() or i == '.'):
+                is_valid = False
+
+    return is_valid
+
+
+def get_number(i):
+
+    mensagem = ['Primeiro', 'Segundo', 'Terceiro']
+    while True:
+        user_number = input(f'{mensagem[i]} segmento: ').replace(',', '.')
+
+        if validate_float_number(user_number):
+            return float(user_number)
+        else:
+            print('Valor inválido.')
+
+
+def check_triangle(side1, side2, side3):
+    return (side1 + side2 > side3) and (side2 + side3 > side1) and (side1 + side3 > side2)
+
+
+def get_category(side1, side2, side3):
+    if side1 == side2 == side3:
+        return 'EQUILÁTERO'
+    elif side1 != side2 != side3:
+        return 'ESCALENO'
+    else:
+        return 'ISÓSCELES'
+
+
+print('-='*18)
+print('Analisador de Triângulos')
+print('-='*18)
+
+print('Digite abaixo o comprimento de cada segmento do triângulo.')
+
+sides = [get_number(0), get_number(1), get_number(2)]
+
+if check_triangle(sides[0], sides[1], sides[2]):
+    category = get_category(sides[0], sides[1], sides[2])
+    print(f"Os segmentos acima PODEM formar um triângulo {category}!")
+else:
+    print("Os segmentos acima NÃO PODEM formar um triângulo!")
+
+
