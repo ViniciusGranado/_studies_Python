ebf3f0fbc5c9aeb500844a352d77325f2d72944a
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-05-08 13:41:30 -0300

    Added solving of exericse 051

---
+++ _exercises/exercise051/README.md
@@ -0,0 +1,7 @@
+# Exercício 051
+
+Escreva um algoritmo que leia o primeiro termo e a razão de uma PA(progressão aritmética).
+No final, mostre os 10 primeiros termos dessa progressão.
+
+Write an algorithm that reads the first term and the common difference of an AP
+(arithmetic progression). In the end, shows the first 10 terms of this progression.
---
+++ _exercises/exercise051/exercise051.py
@@ -0,0 +1,24 @@
+def get_number(index):
+    message = ['Primeiro termo: ', 'Razão: ']
+    while True:
+        user_number = input(message[index])
+
+        if user_number.isnumeric():
+            return int(user_number)
+        else:
+            print('Valor inválido.')
+
+
+print('-'*30)
+print('{:^30}'.format('10 TERMOS DE UMA PA'))
+print('-'*30)
+
+first_term = get_number(0)
+common_difference = get_number(1)
+
+terms = first_term
+for i in range(0, 10):
+    print(terms, end=' ')
+    if i != 9:
+        print('\u279E', end=' ')
+    terms += common_difference
